525229bc6a03a1f1d83afa71c037127de257487e
Ben Duran <benjamind10@pm.me>
2024-01-17 20:37:17 -0500

    added simple bar chart working (sort of?)

---
+++ app/components/Charts/BarChart.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import 'chart.js/auto';
+import { ChartData, ChartOptions } from 'chart.js';
+import { Link } from '@remix-run/react';
+import { LinksFunction } from '@remix-run/node';
+import styles from '~/components/Charts/Charts.css';
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: styles }];
+};
+// Specify the type for 'data' as ChartData for a bar chart
+interface BarChartProps {
+  data: ChartData<'bar'>; // Specify 'bar' for bar chart
+  options: ChartOptions<'bar'>; // Make sure options match the chart type
+}
+
+const BarChart: React.FC<BarChartProps> = ({ data, options }) => {
+  return <Bar data={data} options={options} />;
+};
+
+export default BarChart;
