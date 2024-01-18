28f1be660b6e406bb0e3288dfba54de2513d2468
Ben Duran <ben@bytebug.io>
2024-01-18 17:33:43 +0000

    added game type to score card

--- app/components/ScoreCard/ScoreCard.tsx
+++ app/components/ScoreCard/ScoreCard.tsx
@@ -7,61 +7,66 @@ import styles from './ScoreCard.css';
 import axios from 'axios';
 
 type ScoreCardProps = {
-  initialScores: Score[];
+    initialScores: Score[];
 };
 
 export const links: LinksFunction = () => {
-  return [{ rel: 'stylesheet', href: styles }];
+    return [{ rel: 'stylesheet', href: styles }];
 };
 function ScoreCard({ initialScores }: ScoreCardProps) {
-  const [scores, setScores] = useState(initialScores);
+    const [scores, setScores] = useState(initialScores);
 
-  const handleDelete = async (gameId: undefined | string) => {
-    try {
-      const response = await axios.delete(`/api/delete/${gameId}`);
-      if (response.status === 200) {
-        const updatedScores = scores.filter((score) => score.id !== gameId);
-        setScores(updatedScores);
-      } else {
-        console.error('Failed to delete score');
-      }
-    } catch (error) {
-      console.error('Failed to delete score', error);
-    }
-  };
+    const handleDelete = async (gameId: undefined | string) => {
+        try {
+            const response = await axios.delete(`/api/delete/${gameId}`);
+            if (response.status === 200) {
+                const updatedScores = scores.filter(
+                    (score) => score.id !== gameId
+                );
+                setScores(updatedScores);
+            } else {
+                console.error('Failed to delete score');
+            }
+        } catch (error) {
+            console.error('Failed to delete score', error);
+        }
+    };
 
-  return (
-    <div id="score-card-container">
-      {scores.map((score) => (
-        <article key={score.gameId} className="score-card">
-          <header>
-            <Link to={`/scores/${score.gameId}`}>
-              <h2>Game ID: {score.gameId}</h2>
-            </Link>
-          </header>
-          <p>
-            {score.player1} vs {score.player2}
-          </p>
-          <p>
-            Score: {score.score1} - {score.score2}
-          </p>
-          <p>Winner: {score.winner}</p>
-          <p>Loser: {score.loser}</p>
-          <p>
-            First Serve:
-            {score.firstServe === 'Player1' ? score.player1 : score.player2}
-          </p>
-          <p>Date: {score.createdAt}</p>
-          <button
-            onClick={() => handleDelete(score.id)}
-            className="delete-button"
-          >
-            Delete
-          </button>
-        </article>
-      ))}
-    </div>
-  );
+    return (
+        <div id="score-card-container">
+            {scores.map((score) => (
+                <article key={score.gameId} className="score-card">
+                    <header>
+                        <Link to={`/scores/${score.gameId}`}>
+                            <h2>Game ID: {score.gameId}</h2>
+                            <h3>Game Type: {score.gameType}</h3>
+                        </Link>
+                    </header>
+                    <p>
+                        {score.player1} vs {score.player2}
+                    </p>
+                    <p>
+                        Score: {score.score1} - {score.score2}
+                    </p>
+                    <p>Winner: {score.winner}</p>
+                    <p>Loser: {score.loser}</p>
+                    <p>
+                        First Serve:
+                        {score.firstServe === 'Player1'
+                            ? score.player1
+                            : score.player2}
+                    </p>
+                    <p>Date: {score.createdAt}</p>
+                    <button
+                        onClick={() => handleDelete(score.id)}
+                        className="delete-button"
+                    >
+                        Delete
+                    </button>
+                </article>
+            ))}
+        </div>
+    );
 }
 
 export default ScoreCard;
