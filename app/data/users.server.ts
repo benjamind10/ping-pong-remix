3d8c6360e8770c39d1428294a1baf634df03b67a
Ben Duran <benjamind10@pm.me>
2024-01-29 16:29:35 -0500

    added some validation to games

--- app/data/users.server.ts
+++ app/data/users.server.ts
@@ -1,35 +1,32 @@
 import { prisma } from '~/data/database.server';
 
 export async function getUserByUsername(username: string) {
-  try {
-    const user = await prisma.user.findUnique({
-      where: { username },
-    });
-    return user;
-  } catch (error) {
-    console.error('Error fetching user by username:', error);
-    throw new Error('Failed to fetch user');
-  }
+    try {
+        return await prisma.user.findUnique({
+            where: { username },
+        });
+    } catch (error) {
+        console.error('Error fetching user by username:', error);
+        throw new Error('Failed to fetch user');
+    }
 }
 
 export async function getAllUsers() {
-  try {
-    const users = await prisma.user.findMany();
-    return users;
-  } catch (error) {
-    console.error('Error fetching all users:', error);
-    throw new Error('Failed to fetch users');
-  }
+    try {
+        return await prisma.user.findMany();
+    } catch (error) {
+        console.error('Error fetching all users:', error);
+        throw new Error('Failed to fetch users');
+    }
 }
 
 export async function getUserById(userId: string) {
-  try {
-    const user = await prisma.user.findUnique({
-      where: { id: userId },
-    });
-    return user;
-  } catch (error) {
-    console.error('Error fetching user by ID:', error);
-    throw new Error('Failed to fetch user by ID');
-  }
+    try {
+        return await prisma.user.findUnique({
+            where: { id: userId },
+        });
+    } catch (error) {
+        console.error('Error fetching user by ID:', error);
+        throw new Error('Failed to fetch user by ID');
+    }
 }
